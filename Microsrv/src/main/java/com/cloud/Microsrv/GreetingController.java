07a616c3a2156f96819e5d5281a64491c19fdb91
Alejandro Polo Rangel <alejandro.polo@ext.aqualogybs.net>
2016-05-06 14:21:11 +0200

    inicial

---
+++ Microsrv/src/main/java/com/cloud/Microsrv/GreetingController.java
@@ -0,0 +1,14 @@
+package com.cloud.Microsrv;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class GreetingController {
+
+	private static final String template = "Hello, %s!";
+
+	@RequestMapping("/greeting/{name}")
+	public Greeting greeting(@PathVariable("name") String name) {
+		return new Greeting(String.format(template, name));
+	}
+}
